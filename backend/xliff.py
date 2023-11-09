47aea2707ade501936f4b37adafd65be658bb376
Denis Bezykornov <archidevil52@gmail.com>
2023-11-10 01:22:29 +0300

    Add support for additional attributes

--- backend/xliff.py
+++ backend/xliff.py
@@ -1,10 +1,25 @@
+from typing import Optional
 from xml.etree import ElementTree
+from xml.etree.ElementTree import Element
 
 
 class XliffSegment:
-    def __init__(self, source: str, target: str) -> None:
+    def __init__(
+        self, id_: int, approved: Optional[bool], source: str, target: str
+    ) -> None:
+        self.__id = id_
+        self.__approved = approved if approved else False
         self.__source = source
         self.__target = target
+        self.__dirty = False
+
+    @property
+    def id_(self) -> int:
+        return self.__id
+
+    @property
+    def approved(self) -> bool:
+        return self.__approved
 
     @property
     def original(self) -> str:
@@ -14,24 +29,48 @@ class XliffSegment:
     def translation(self) -> str:
         return self.__target
 
+    @property
+    def dirty(self) -> bool:
+        return self.__dirty
+
     @translation.setter
     def translation(self, value: str) -> None:
-        # TODO put data into XML document by ID
         self.__target = value
+        self.__dirty = True
+
+    @approved.setter
+    def approved(self, value: bool) -> None:
+        self.__approved = value
+        self.__dirty = True
 
     def __str__(self) -> str:
-        return f"XliffSegment({self.__source}, {self.__target})"
+        return f"XliffSegment({self.__id}, {self.__approved}, {self.__source}, {self.__target})"
 
     def __repr__(self) -> str:
         return self.__str__()
 
 
 class XliffData:
-    def __init__(self, segments: list[XliffSegment]) -> None:
+    def __init__(self, segments: list[XliffSegment], xliff_file: Element) -> None:
         self.__segments = segments
+        self.__xliff_file = xliff_file
 
+    @property
     def segments(self) -> list[XliffSegment]:
         return self.__segments
+
+    @property
+    def xliff_file(self) -> Element:
+        return self.__xliff_file
+
+    def commit(self) -> None:
+        # go over segments and apply changes to translation and accepted attributes
+        # if segment is dirty
+        for segment in self.__segments:
+            if not segment.dirty:
+                continue
+
+        # TODO: find node and update it
 
 
 # this is 1.2 version parser as SmartCAT supports only this version
@@ -44,12 +83,19 @@ def extract_xliff_content(content: str) -> XliffData:
 
     # TODO: support XLIFF namespace urn:oasis:names:tc:xliff:document:1.2
     # TODO: support XLIFFs without namespaces
-    # TODO: support accepted attribute
 
     segments: list[XliffSegment] = []
     for unit in root.iter("{urn:oasis:names:tc:xliff:document:1.2}trans-unit"):
+        segment_id = unit.attrib.get("id")
+        approved = unit.attrib.get("approved") == "yes"
         src_segment = unit.find("{urn:oasis:names:tc:xliff:document:1.2}source")
         tgt_segment = unit.find("{urn:oasis:names:tc:xliff:document:1.2}target")
+
+        if not segment_id:
+            print("Error: <unit> does not have id attribute", unit.text)
+            continue
+
+        segment_id = int(segment_id)
 
         if src_segment is None or not src_segment.text:
             print("Error: <unit> does not have <source>", unit.text)
@@ -59,7 +105,8 @@ def extract_xliff_content(content: str) -> XliffData:
             print("Error: <unit> does not have <target>", unit.text)
             continue
 
-        # TODO collect XML data instead of raw values
-        segments.append(XliffSegment(src_segment.text, tgt_segment.text))
+        segments.append(
+            XliffSegment(segment_id, approved, src_segment.text, tgt_segment.text)
+        )
 
-    return XliffData(segments)
+    return XliffData(segments, root)
