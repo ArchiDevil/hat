0cf2aecb6b06eb5e531b9b09016f61cae8bb5423
Denis Bezykornov <archidevil52@gmail.com>
2023-12-27 00:07:43 +0300

    Add simple set of tests on XLIFF processor

---
+++ backend/tests/test_xliff_bp.py
@@ -0,0 +1,71 @@
+from quart.testing import QuartClient
+from quart.datastructures import FileStorage
+
+
+from app.db import get_session
+from app.schema import XliffDocument, XliffRecord
+
+
+async def test_can_show_xliff(client: QuartClient):
+    async with client.app.app_context():
+        with get_session() as session:
+            records = [
+                XliffRecord(source="test1", target="test1"),
+                XliffRecord(source="test2", target="test2"),
+            ]
+            session.add(XliffDocument(name="test", records=records))
+            session.commit()
+
+    response = await client.get("/xliff/1")
+    assert response.status_code == 200
+    assert "test1" in (await response.data).decode("utf-8")
+    assert "test2" in (await response.data).decode("utf-8")
+
+
+async def test_shows_404_for_unknown_xliff(client):
+    response = await client.get("/xliff/1")
+    assert response.status_code == 404
+
+
+async def test_upload(client):
+    with open("tests/small.xliff", "rb") as fp:
+        response = await client.post(
+            "/xliff/upload", files={"xliff-file": FileStorage(stream=fp)}
+        )
+    assert response.status_code == 302
+    assert "xliff/1" in response.location
+
+    async with client.app.app_context():
+        with get_session() as session:
+            doc = session.query(XliffDocument).filter_by(id=1).first()
+            assert doc is not None
+            assert doc.name == "test"
+            assert len(doc.records) == 1
+
+
+async def test_upload_no_file(client):
+    response = await client.post("/xliff/upload", files={})
+    assert response.status_code == 400
+
+
+async def test_delete(client):
+    async with client.app.app_context():
+        with get_session() as session:
+            records = [
+                XliffRecord(source="test1", target="test1"),
+                XliffRecord(source="test2", target="test2"),
+            ]
+            session.add(XliffDocument(name="test", records=records))
+            session.commit()
+
+    response = await client.get("/xliff/1/delete")
+    assert response.status_code == 302
+
+    async with client.app.app_context():
+        with get_session() as session:
+            assert session.query(XliffDocument).filter_by(id=1).first() is None
+
+
+async def test_delete_not_found(client):
+    response = await client.get("/xliff/1/delete")
+    assert response.status_code == 404
