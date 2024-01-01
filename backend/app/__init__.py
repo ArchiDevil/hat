acb434dd0e514ebace80b74d81a475dced0ee232
Denis Bezykornov <archidevil52@gmail.com>
2024-01-01 23:49:41 +0300

    Add CORS for dev server

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,6 @@
 import os
 from quart import Quart
+from quart_cors import cors
 
 from app import index
 from app.bps import tmx, xliff, api
@@ -7,6 +8,9 @@ from app.bps import tmx, xliff, api
 
 def create_app(mode="Production", additional_config=None):
     app = Quart(__name__)
+
+    if app.debug:
+        app = cors(app, allow_origin="*")
 
     app.config.from_object(f"app.settings.{mode}")
     app.config.update(additional_config or {})
@@ -17,9 +21,9 @@ def create_app(mode="Production", additional_config=None):
         )
         app.config["DATABASE"] = db_url
 
-    app.register_blueprint(index.bp)
-    app.register_blueprint(tmx.bp)
-    app.register_blueprint(xliff.bp)
-    app.register_blueprint(api.bp)
+    app.register_blueprint(cors(index.bp, allow_origin="*") if app.debug else index.bp)
+    app.register_blueprint(cors(tmx.bp, allow_origin="*") if app.debug else tmx.bp)
+    app.register_blueprint(cors(xliff.bp, allow_origin="*") if app.debug else xliff.bp)
+    app.register_blueprint(cors(api.bp, allow_origin="*") if app.debug else api.bp)
 
     return app
