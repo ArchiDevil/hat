4732008ec97c9438a3bb3aa2d8d27033cd6e9709
Denis Bezykornov <archidevil52@gmail.com>
2024-03-07 00:51:03 +0300

    Add very preliminary support for files sent through API

---
+++ frontend/generator/interfaces.ts
@@ -0,0 +1,78 @@
+export type RefDesc = {
+  $ref: string
+}
+
+export type AnyOfDesc = {
+  anyOf: [
+    {
+      type: 'string' | 'integer' // | 'boolean' // check if boolean is possible
+    }
+  ]
+}
+
+export type TrivialDesc = {
+  type: 'string' | 'integer' // | 'boolean' // check if boolean is possible
+  title?: string
+  format?: string
+}
+
+export type ArrayDesc = {
+  type: 'array'
+  items: RefDesc | AnyOfDesc
+  title?: string
+}
+
+// TODO: check if AnyOfDesc is possible here without an array
+export type PropDescription = TrivialDesc | ArrayDesc | RefDesc
+
+export type HttpMethod = 'get' | 'post' | 'put' | 'delete'
+
+export interface ParamDesc {
+  name: string
+  in: 'query' | 'path' | 'header'
+  required: boolean
+  schema: PropDescription
+}
+
+export interface MethodDesc {
+  tags: string[]
+  summary: string
+  operationId: string
+  parameters?: ParamDesc[]
+  requestBody?: any // TODO: describe
+  responses: {
+    [status: string]: {
+      description: string
+      content?: {
+        [contentType: string]: {
+          schema: PropDescription
+        }
+      }
+    }
+  }
+}
+
+export interface ApiDescription {
+  openapi: string
+  info: {
+    title: string
+    version: string
+  }
+  paths: {
+    [path: string]: {
+      [method in HttpMethod]: MethodDesc
+    }
+  }
+  components: {
+    schemas: {
+      [name: string]: {
+        type: string
+        title: string
+        required?: string[]
+        properties: {
+          [name: string]: PropDescription
+        }
+      }
+    }
+  }
+}
