bd69d88dd27494b18eccb705a02b0871cf8b8132
Denis Bezykornov <archidevil52@gmail.com>
2024-06-15 02:46:15 +0300

    Add place for modal

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
+  <div class="content container">
     <router-view />
   </div>
+  <div
+    id="modal"
+    class="content"
+  ></div>
 </template>
+
+<style scoped>
+.content {
+  @apply text-base text-slate-900 font-medium font-sans;
+}
+</style>
