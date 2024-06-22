4d750901428f88ebbb7d2fb1cf548c9c28f0881f
Denis Bezykornov <archidevil52@gmail.com>
2024-06-22 20:31:55 +0300

    Restyle document pair a bit

--- frontend/src/components/DocumentPair.vue
+++ frontend/src/components/DocumentPair.vue
@@ -9,10 +9,10 @@ defineProps<{
 
 <template>
   <div class="grid grid-cols-2 gap-2 mb-1">
-    <p class="border-2 rounded p-2">
+    <p class="border rounded-border border-surface p-2">
       {{ record.source }}
     </p>
-    <p class="border-2 rounded p-2">
+    <p class="border rounded-border border-surface p-2">
       {{ record.target }}
     </p>
   </div>
