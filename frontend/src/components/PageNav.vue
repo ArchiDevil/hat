bbdcf05fd5be0aee0bd5043e36b6acb55b49d7e6
Denis Bezykornov <archidevil52@gmail.com>
2024-06-28 21:44:43 +0300

    Extract PageNav header component

---
+++ frontend/src/components/PageNav.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import {useRouter} from 'vue-router'
+
+import {useUserStore} from '../stores/user'
+
+import PageTitle from './PageTitle.vue'
+import RoutingLink from './RoutingLink.vue'
+
+const router = useRouter()
+
+const logout = async () => {
+  await useUserStore().logout()
+  router.push({name: 'login'})
+}
+</script>
+
+<template>
+  <div class="flex items-baseline">
+    <PageTitle
+      class="flex-grow"
+      title="Human Assisted Translation project"
+    />
+    <div class="pt-8">
+      <RoutingLink
+        class="mx-2 uppercase font-semibold"
+        href="/"
+      >
+        Home
+      </RoutingLink>
+      <RoutingLink
+        v-if="useUserStore().currentUser?.role === 'admin'"
+        class="mx-2 uppercase font-semibold"
+        href="/users/"
+      >
+        Users
+      </RoutingLink>
+      <a
+        href="#"
+        class="mx-2 uppercase font-semibold underline hover:decoration-2"
+        @click="logout"
+      >
+        Logout
+      </a>
+    </div>
+  </div>
+</template>
