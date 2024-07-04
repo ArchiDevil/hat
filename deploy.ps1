77a9bc38cda0580b063b94bcc949fd1efe5c3698
Denis Bezykornov <archidevil52@gmail.com>
2024-07-05 01:15:00 +0300

    Move host setting to the correct place after docker update

--- deploy.ps1
+++ deploy.ps1
@@ -1,1 +1,1 @@
-docker-compose --env-file .env.prod --host ssh://root@192.168.1.72 up --build -d --remove-orphans
+docker --host ssh://root@192.168.1.72 compose --env-file .env.prod up --build -d --remove-orphans
