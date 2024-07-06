8f4c30e789a945e380233f4aaef787a641be2f52
Denis Bezykornov <archidevil52@gmail.com>
2024-07-06 17:52:09 +0300

    Add pagination for TMX viewer

--- frontend/src/views/TmxView.vue
+++ frontend/src/views/TmxView.vue
@@ -1,16 +1,39 @@
 <script setup lang="ts">
-import {onMounted, ref} from 'vue'
-import {useRoute} from 'vue-router'
+import {computed, onMounted, ref, watchEffect} from 'vue'
+import {useRoute, useRouter} from 'vue-router'
 
-import {getTmx} from '../client/services/TmxService'
+import {getTmx, getTmxRecords} from '../client/services/TmxService'
 import {TmxFileWithRecordsCount} from '../client/schemas/TmxFileWithRecordsCount'
 import {TmxFileRecord} from '../client/schemas/TmxFileRecord'
+
+import Paginator, {PageState} from 'primevue/paginator'
 
 import DocumentPair from '../components/DocumentPair.vue'
 import PageTitle from '../components/PageTitle.vue'
 
+const route = useRoute()
+const router = useRouter()
+
 const document = ref<TmxFileWithRecordsCount>()
 const records = ref<TmxFileRecord[]>()
+const page = computed(() => {
+  return route.query['page'] ? parseInt(route.query['page'] as string) : 0
+})
+
+watchEffect(async () => {
+  if (!document.value) {
+    return
+  }
+  records.value = await getTmxRecords(document.value.id, page.value)
+})
+
+const updatePage = async (event: PageState) => {
+  router.push({
+    query: {
+      page: event.page,
+    },
+  })
+}
 
 onMounted(async () => {
   const route = useRoute()
@@ -24,12 +47,24 @@ onMounted(async () => {
     <p>File ID: {{ document?.id }}</p>
     <p>File name: {{ document?.name }}</p>
     <p class="mb-4">Number of records: {{ document?.records_count }}</p>
+    <Paginator
+      :rows="100"
+      :total-records="document?.records_count"
+      :first="page * 100"
+      v-on:page="(event) => updatePage(event)"
+    />
     <div v-if="records">
       <DocumentPair
         :record="record"
-        v-for="record in records"
-        :key="record.source"
+        v-for="(record, i) in records"
+        :key="i"
       />
     </div>
+    <Paginator
+      :rows="100"
+      :total-records="document?.records_count"
+      :first="page * 100"
+      v-on:page="(event) => updatePage(event)"
+    />
   </div>
 </template>
