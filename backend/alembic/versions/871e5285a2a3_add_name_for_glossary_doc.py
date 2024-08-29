d1f6137f8cd3068da408957d0ec8466a41988ae2
Denis Bezykornov <archidevil52@gmail.com>
2024-08-30 01:53:31 +0300

    Add better data handling for migration scripts in glossaries

--- backend/alembic/versions/871e5285a2a3_add_name_for_glossary_doc.py
+++ backend/alembic/versions/871e5285a2a3_add_name_for_glossary_doc.py
@@ -11,6 +11,7 @@ from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 
+# pylint: disable=E1101
 
 revision: str = "871e5285a2a3"
 down_revision: Union[str, None] = "96bf2e203c03"
@@ -19,7 +20,9 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.add_column("glossary_document", sa.Column("name", sa.String(), nullable=False))
+    op.add_column("glossary_document", sa.Column("name", sa.String()))
+    op.execute(sa.update(sa.table("glossary_document", sa.column('name'))).values(name='empty'))
+    op.alter_column("glossary_document", "name", nullable=False)
 
 
 def downgrade() -> None:
--- backend/alembic/versions/c8d6d3fa5dda_rename_glossary_doc_to_glossary.py
+++ backend/alembic/versions/c8d6d3fa5dda_rename_glossary_doc_to_glossary.py
@@ -10,13 +10,36 @@ from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
-from sqlalchemy.dialects import postgresql
+
+# pylint: disable=E1101
 
 revision: str = "c8d6d3fa5dda"
 down_revision: Union[str, None] = "871e5285a2a3"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
+
+gt = sa.table(
+    'glossary',
+    sa.column('id'),
+    sa.column('created_at'),
+    sa.column('updated_at'),
+    sa.column('name'),
+    sa.column('processing_status'),
+    sa.column('upload_time'),
+    sa.column('user_id'),
+)
+
+gdt = sa.table(
+    'glossary_document',
+    sa.column('id'),
+    sa.column('created_at'),
+    sa.column('updated_at'),
+    sa.column('name'),
+    sa.column('processing_status'),
+    sa.column('upload_time'),
+    sa.column('user_id'),
+)
 
 def upgrade() -> None:
     op.create_table(
@@ -34,13 +57,35 @@ def upgrade() -> None:
         ),
         sa.PrimaryKeyConstraint("id"),
     )
+
+    insert = sa.insert(gt).from_select(
+        [
+            'id', 'created_at', 'updated_at', 'name',
+            'processing_status', 'upload_time', 'user_id'
+        ],
+        sa.select(
+            gdt.c.id, gdt.c.created_at, gdt.c.updated_at, gdt.c.name,
+            gdt.c.processing_status, gdt.c.upload_time, gdt.c.user_id
+        )
+    )
+    op.execute(insert)
     op.drop_constraint(
         "glossary_record_document_id_fkey", "glossary_record", type_="foreignkey"
     )
     op.drop_table("glossary_document")
     op.add_column(
-        "glossary_record", sa.Column("glossary_id", sa.Integer(), nullable=False)
+        "glossary_record", sa.Column("glossary_id", sa.Integer())
     )
+    op.execute(
+        sa.update(
+            sa.table(
+                "glossary_record",
+                sa.column('glossary_id'),
+                sa.column('document_id')
+            )
+        ).values(glossary_id=sa.column('document_id'))
+    )
+    op.alter_column('glossary_record', 'glossary_id', nullable=False)
 
     op.create_foreign_key(None, "glossary_record", "glossary", ["glossary_id"], ["id"])
     op.drop_column("glossary_record", "document_id")
@@ -49,33 +94,52 @@ def upgrade() -> None:
 def downgrade() -> None:
     op.add_column(
         "glossary_record",
-        sa.Column("document_id", sa.INTEGER(), autoincrement=False, nullable=False),
+        sa.Column("document_id", sa.Integer(), autoincrement=False),
     )
+    op.execute(
+        sa.update(
+            sa.table("glossary_record",
+                     sa.column('glossary_id'),
+                     sa.column('document_id')
+                    )
+            ).values(document_id=sa.column('glossary_id')))
+    op.alter_column('glossary_record', 'document_id', nullable=False)
     op.drop_constraint("glossary_record_glossary_id_fkey", "glossary_record", type_="foreignkey")
 
     op.drop_column("glossary_record", "glossary_id")
     op.create_table(
         "glossary_document",
-        sa.Column("id", sa.INTEGER(), autoincrement=True, nullable=False),
+        sa.Column("id", sa.Integer(), autoincrement=True, nullable=False),
         sa.Column(
-            "created_at", postgresql.TIMESTAMP(), autoincrement=False, nullable=False
+            "created_at", sa.DateTime(), autoincrement=False, nullable=False
         ),
         sa.Column(
-            "updated_at", postgresql.TIMESTAMP(), autoincrement=False, nullable=False
+            "updated_at", sa.DateTime(), autoincrement=False, nullable=False
         ),
         sa.Column(
-            "processing_status", sa.VARCHAR(), autoincrement=False, nullable=False
+            "processing_status", sa.String(), autoincrement=False, nullable=False
         ),
         sa.Column(
-            "upload_time", postgresql.TIMESTAMP(), autoincrement=False, nullable=False
+            "upload_time", sa.DateTime(), autoincrement=False, nullable=False
         ),
-        sa.Column("user_id", sa.INTEGER(), autoincrement=False, nullable=False),
-        sa.Column("name", sa.VARCHAR(), autoincrement=False, nullable=False),
+        sa.Column("user_id", sa.Integer(), autoincrement=False, nullable=False),
+        sa.Column("name", sa.String(), autoincrement=False, nullable=False),
         sa.ForeignKeyConstraint(
             ["user_id"], ["user.id"], name="glossary_document_user_id_fkey"
         ),
         sa.PrimaryKeyConstraint("id", name="glossary_document_pkey"),
     )
+    insert = sa.insert(gdt).from_select(
+        [
+            'id', 'created_at', 'updated_at', 'name',
+            'processing_status', 'upload_time', 'user_id'
+        ],
+        sa.select(
+            gt.c.id, gt.c.created_at, gt.c.updated_at, gt.c.name,
+            gt.c.processing_status, gt.c.upload_time, gt.c.user_id
+        )
+    )
+    op.execute(insert)
     op.create_foreign_key(
         "glossary_record_document_id_fkey",
         "glossary_record",
