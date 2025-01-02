98ea65256331e51f478bf721eeb7d3c965c0caf9
Denis Bezykornov <archidevil52@gmail.com>
2025-01-02 03:53:27 +0300

    Migrate to uvicorn from hypercorn

--- backend/run.sh
+++ backend/run.sh
@@ -6,4 +6,4 @@
 alembic upgrade head
 
 # then run the app
-exec hypercorn -b 0.0.0.0:8000 --workers 4 --access-logfile - --error-logfile - main:app
+exec uvicorn main:app --host 0.0.0.0 --port 8000 --workers 4 --ws none
