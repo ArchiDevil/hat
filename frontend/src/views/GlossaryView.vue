afd4f379db8bc6e282b6927e5f3a3cfe21c4443e
Denis Bezykornov <archidevil52@gmail.com>
2025-09-19 23:31:57 +0300

    Make proper columns for glossary view

--- frontend/src/views/GlossaryView.vue
+++ frontend/src/views/GlossaryView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import {onMounted, ref} from 'vue'
 import {useRoute} from 'vue-router'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
 
-import DocSegment from '../components/DocSegment.vue'
 import PageTitle from '../components/PageTitle.vue'
 import PageNav from '../components/PageNav.vue'
+
 import {GlossaryResponse} from '../client/schemas/GlossaryResponse'
 import {retrieveGlossary, listRecords} from '../client/services/GlossaryService'
 import {GlossaryRecordSchema} from '../client/schemas/GlossaryRecordSchema'
@@ -25,20 +27,32 @@ onMounted(async () => {
     <PageTitle title="Glossary viewer" />
     <p>File ID: {{ document?.id }}</p>
     <p>File name: {{ document?.name }}</p>
-    <p class="mb-4">
-      Number of records: {{ records?.length }}
-    </p>
+    <p class="mb-4">Number of records: {{ records?.length }}</p>
     <div
       v-if="records"
       class="flex flex-col gap-1"
     >
-      <DocSegment
-        v-for="record in records"
-        :id="record.id"
-        :key="record.id"
-        :source="record.source"
-        :target="record.target"
-      />
+      <DataTable
+        :value="records"
+        size="small"
+      >
+        <Column
+          field="source"
+          header="Source"
+        />
+        <Column
+          field="target"
+          header="Target"
+        />
+        <Column
+          field="comment"
+          header="Comment"
+        />
+        <Column
+          :field="(record: GlossaryRecordSchema) => new Date(record.updated_at).toLocaleString()"
+          header="Last update"
+        />
+      </DataTable>
     </div>
   </div>
 </template>
