5a83eeb9eb8819c47a33695352f54fc6b6e666ff
Denis Bezykornov <archidevil52@gmail.com>
2025-09-20 01:08:32 +0300

    Update mocks to the new scheme

--- frontend/mocks/glossaryMocks.ts
+++ frontend/mocks/glossaryMocks.ts
@@ -5,30 +5,31 @@ import {
   retrieveGlossary,
 } from '../src/client/services/GlossaryService'
 import {type AwaitedReturnType} from './utils'
+import {defaultUser} from './userMocks'
 
 export const glossaryMocks = [
   http.get('http://localhost:8000/glossary/', () =>
     HttpResponse.json<AwaitedReturnType<typeof listGlossary>>([
       {
         id: 51,
-        created_by: 12,
         name: 'Some glossary',
         created_at: '2024-12-03T12:31:22',
         updated_at: '2024-12-03T16:32:22',
         processing_status: 'done',
         upload_time: '2024-12-03T12:32:05',
+        created_by_user: defaultUser,
       },
     ])
   ),
   http.get('http://localhost:8000/glossary/:id', ({params}) =>
     HttpResponse.json<AwaitedReturnType<typeof retrieveGlossary>>({
       id: Number(params.id),
-      created_by: 12,
       name: 'Some glossary',
       created_at: '2024-12-03T12:31:22',
       updated_at: '2024-12-03T16:32:22',
       processing_status: 'done',
       upload_time: '2024-12-03T12:32:05',
+      created_by_user: defaultUser,
     })
   ),
   http.get('http://localhost:8000/glossary/:id/records', ({params}) =>
@@ -36,22 +37,22 @@ export const glossaryMocks = [
       {
         id: 1,
         glossary_id: Number(params.id),
-        created_by: 12,
         created_at: '2024-12-03T12:31:22',
         updated_at: '2024-12-03T12:31:22',
         source: 'Some source',
         target: 'Some target',
         comment: 'This is a comment',
+        created_by_user: defaultUser,
       },
       {
         id: 2,
         glossary_id: Number(params.id),
-        created_by: 12,
         created_at: '2024-12-03T12:31:22',
         updated_at: '2024-12-03T12:31:22',
         source: 'Another source',
         target: 'Another target',
         comment: 'Some comment from another segment',
+        created_by_user: defaultUser,
       },
     ])
   ),
