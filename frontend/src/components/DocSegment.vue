29f74f05bb45d172dd6631ab622ec758882e7d91
Denis Bezykornov <archidevil52@gmail.com>
2025-12-07 23:57:13 +0300

    Add UI to show segment sources

--- frontend/src/components/DocSegment.vue
+++ frontend/src/components/DocSegment.vue
@@ -4,6 +4,7 @@ import {computed, ref, useTemplateRef, watch} from 'vue'
 import Button from 'primevue/button'
 
 import {cleanableDebounce} from '../utilities/utils'
+import {RecordSource} from '../client/schemas/RecordSource'
 
 const props = defineProps<{
   id: number
@@ -15,6 +16,7 @@ const props = defineProps<{
   approved?: boolean
   repetitionsCount?: number
   hasComments?: boolean
+  recordSrc?: RecordSource
 }>()
 
 const emit = defineEmits<{
@@ -74,6 +76,32 @@ const icon = computed(
 const showCommentsDialog = () => {
   emit('addComment')
 }
+
+const segSourceTitle = computed(() => {
+  switch (props.recordSrc) {
+    case 'glossary':
+      return 'Glossary term'
+    case 'mt':
+      return 'Machine translation'
+    case 'tm':
+      return 'Translation memory'
+    default:
+      return undefined
+  }
+})
+
+const segSourceIcon = computed(() => {
+  switch (props.recordSrc) {
+    case 'glossary':
+      return 'pi-globe'
+    case 'mt':
+      return 'pi-language'
+    case 'tm':
+      return 'pi-database'
+    default:
+      return undefined
+  }
+})
 </script>
 
 <template>
@@ -118,9 +146,20 @@ const showCommentsDialog = () => {
   </div>
   <div
     v-if="editable"
-    class="flex flex-row text-center w-20 self-start gap-2"
+    class="flex flex-row text-center self-start gap-2 pr-2 h-full"
   >
+    <div
+      v-if="recordSrc && recordSrc !== 'user'"
+      class="py-1 px-2"
+      :title="segSourceTitle"
+    >
+      <i
+        class="pi text-surface-500"
+        :class="segSourceIcon"
+      />
+    </div>
     <Button
+      class="ml-auto"
       :icon="icon"
       rounded
       outlined
