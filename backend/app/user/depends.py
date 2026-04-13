cb06bf9ee7cdbed5456035314a5780c44ac962e5
Denis Bezykornov <archidevil52@gmail.com>
2026-04-14 01:47:34 +0300

    Remove dead code

--- backend/app/user/depends.py
+++ backend/app/user/depends.py
@@ -1,11 +1,8 @@
 from typing import Annotated
 
-from fastapi import Cookie, Depends, HTTPException, status
+from fastapi import Cookie, HTTPException, status
 from itsdangerous import URLSafeTimedSerializer
-from sqlalchemy.orm import Session
 
-from app import models, schema
-from app.db import get_db
 from app.settings import settings
 
 
@@ -21,30 +18,3 @@ def get_current_user_id(
         return data["user_id"]
 
     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-
-
-class RoleChecker:
-    def __init__(self, role: models.UserRole) -> None:
-        self.__role = role
-
-    def __call__(
-        self,
-        user_id: Annotated[int, Depends(get_current_user_id)],
-        db: Annotated[Session, Depends(get_db)],
-    ):
-        user = db.query(schema.User).filter_by(id=user_id).first()
-        if not user:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-
-        if self.__role == models.UserRole.ADMIN and user.role == "admin":
-            return
-
-        if self.__role == models.UserRole.USER and user.role in ["user", "admin"]:
-            return
-
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
-
-
-# TODO: do we need a moderator role in the future?
-has_admin_role = RoleChecker(models.UserRole.ADMIN)
-has_user_role = RoleChecker(models.UserRole.USER)
